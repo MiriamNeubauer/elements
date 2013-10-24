68bd1a82d949d8fc0bd547c018c18e9c3063f0c1
sjgjohnston <johnston.sjg@gmail.com>
2013-10-23 23:49:54 -0700

    Adding the server side facebook authentication and updates to the initial index.html.erb sign-up view and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Elements::Application.routes.draw do
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
   root to: "site#index"
 
 end
